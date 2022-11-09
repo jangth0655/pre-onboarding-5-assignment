a1068f958c0d32af07fbd8ef049597ef185fae62
taehee <jangth0655@gmail.com>
2022-11-09 21:19:50 +0900

    [feat] input 입력과 submit기능 추가

--- src/components/search/form/SearchForm.tsx
+++ src/components/search/form/SearchForm.tsx
@@ -1,18 +1,45 @@
+// 단어 -> 보드 : 상태관리가 필요하다. (context api)
+// ?클릭 -> 전체 레이아웃, 타이틀 클릭시 active 활성황 여부  (context api)
+
+import React, { useState } from 'react';
 import { BiSearch } from 'react-icons/bi';
 import SearchBoard from '../board/SearchBoard';
 import S from './styles';
 
 const SearchForm = () => {
+  const [active, setActive] = useState(false);
+  const [term, setTerm] = useState('');
+
+  const handleInputEvent = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = e;
+    setTerm(value);
+    setActive(true);
+  };
+
+  const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setTerm('');
+  };
+
+  console.log(active);
   return (
     <>
-      <S.Form>
+      <S.Form onSubmit={handleSubmit}>
         <S.IconBox>
           <BiSearch />
         </S.IconBox>
-        <S.Input type="text" placeholder="질환명을 입력해주세요." />
-        <S.Button type="button">검색</S.Button>
+        <S.Input
+          onClick={() => setActive(true)}
+          onChange={handleInputEvent}
+          value={term}
+          type="text"
+          placeholder="질환명을 입력해주세요."
+        />
+        <S.Button>검색</S.Button>
       </S.Form>
-      <SearchBoard />
+      {active ? <SearchBoard /> : ''}
     </>
   );
 };
